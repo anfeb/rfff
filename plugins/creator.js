fcb0e5095d559e90097533f5f4aa78fb84256dee
anfeb <88554978+anfeb@users.noreply.github.com>
2022-01-07 01:56:28 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -7,7 +7,7 @@ async function handler(m) {
                     + 'ORG:Ilman;\n' // the organization of the contact
                     + 'TEL;type=CELL;type=VOICE;waid=6287773973701:+62 877-7397-3701\n' // WhatsApp ID + phone number
                     + 'END:VCARD'
-                conn.sendMessage(m.chat, { contacts: { displayName: 'Ilman', contacts: [{ vcard }] } }, { quoted: m })
+                conn.sendMessage(m.chat, { contacts: { displayName: 'anfebn', contacts: [{ vcard }] } }, { quoted: m })
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
