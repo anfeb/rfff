68f53baf05f4e074f8e24b6c1ffa31941bf211ba
anfeb <88554978+anfeb@users.noreply.github.com>
2022-01-07 02:45:17 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,19 +1,17 @@
-let handler = async function (m, { conn }) {
-  let list = []
-  for (let i of owner.map(v => v + '@s.whatsapp.net')) {
-    let name = db.data.users[i] ? db.data.users[i].name : conn.getName(i)
-    list.push({
-      "displayName": name,
-      "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:;${name};;;\nFN:${name}\nitem1.TEL;waid=${i.split('@')[0]}:${i.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
-    })
-  }
-  await conn.sendMessage(m.chat, {
-    "displayName": `${list.length} Contact`,
-    "contacts": list
-  }, 'contactsArrayMessage', { quoted: m })
+const PhoneNumber = require('awesome-phonenumber')
+async function handler(m) {
+                let vcard = 'BEGIN:VCARD\n' // metadata of the contact card
+                    + 'VERSION:3.0\n' 
+                    + 'N:;Anfebn;;;'
+                    + 'FN:Anfebn\n' // full name
+                    + 'ORG:Anfebn;\n' // the organization of the contact
+                    + 'TEL;type=CELL;type=VOICE;waid=6287773973701:+62 877-7397-3701\n' // WhatsApp ID + phone number
+                    + 'END:VCARD'
+                conn.sendMessage(m.chat, { contacts: { displayName: 'Anfebn', contacts: [{ vcard }] } }, { quoted: m })
 }
-handler.help = ['owner']
+handler.help = ['owner', 'creator']
 handler.tags = ['info']
+
 handler.command = /^(owner|creator)$/i
 
 module.exports = handler
