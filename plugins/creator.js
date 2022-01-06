d5a87395e5575d9dfaf3b28b84e8b0ad7da99ac9
anfeb <88554978+anfeb@users.noreply.github.com>
2022-01-07 02:36:29 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -2,9 +2,9 @@ const PhoneNumber = require('awesome-phonenumber')
 async function handler(m) {
                 let vcard = 'BEGIN:VCARD\n' // metadata of the contact card
                     + 'VERSION:3.0\n' 
-                    + 'N:;Ilman;;;'
-                    + 'FN:Ilman\n' // full name
-                    + 'ORG:Ilman;\n' // the organization of the contact
+                    + 'N:;anfebn;;;'
+                    + 'FN:anfebn\n' // full name
+                    + 'ORG:anfebn;\n' // the organization of the contact
                     + 'TEL;type=CELL;type=VOICE;waid=6287773973701:+62 877-7397-3701\n' // WhatsApp ID + phone number
                     + 'END:VCARD'
                 conn.sendMessage(m.chat, { contacts: { displayName: 'anfebn', contacts: [{ vcard }] } }, { quoted: m })
